a8b3f855a89323e39a235880d08941ed0b468581
siyabongam26 <siyabongam26@gmail.com>
2024-03-17 03:11:47 -0700

    update

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in session.query(State).filter(State.name.like('%a%')):
-        print(instance.id, instance.name, nov=": ")
+        print(instance.id, instance.name, sep=": ")
