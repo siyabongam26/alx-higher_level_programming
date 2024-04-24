e37fa5a35b6734d567dfd0ef2b30844eea08e4c7
Siyabonga Magagula <siyabongam26@gmail.com.com>
2024-04-23 19:52:57 -0700

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-// Import the built-in Node.js 'fs' module.
 
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  // Use fs.readFile() to read the contents of a file specified as a command-line argument
-  // 'utf8' specifies the encoding of the file being read
-
-  if (error) {
-    // If an error occurs during the file read operation, the 'error' parameter will contain an error object.
-    console.error('Error reading the file:', error);
-
-  } else {
-    // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
-    console.log(content);
-  }
-});
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
